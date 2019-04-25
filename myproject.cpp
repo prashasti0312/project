0960aa9693bdf516f35a839489dc0b42cfb62f69
KHUSHBU KATHURIA <khushbuk2k@gmail.com>
2019-04-25 21:28:35 +0530

    khushbu 5th commit

--- myproject.cpp
+++ myproject.cpp
@@ -558,3 +558,102 @@ cout<<"\n7. Exit";
 cout<<"\n\nEnter Option: ";
 
 cin>>opt;
+switch(opt)
+
+{   case 1:
+
+manageRooms();
+
+break;
+
+case 2:
+
+if(count==0)
+
+{   cout<<"\nRooms data is not available.\nPlease add the rooms first.";
+
+getch();    }
+
+else
+
+hm.checkIn();
+
+break;
+
+case 3:
+
+if(count==0)
+
+{   cout<<"\nRooms data is not available.\nPlease add the rooms first.";
+
+getch();   }
+
+else
+
+hm.getAvailRoom();
+
+break;
+
+case 4:
+
+if(count==0)
+
+{   cout<<"\nRooms are not available.\nPlease add the rooms first.";
+
+getch();    }
+
+else
+
+{  cout<<"Enter Customer Name: ";
+
+cin>>pname;
+
+hm.searchCustomer(pname);    }
+
+break;
+
+case 5:
+
+if(count==0)
+
+{   cout<<"\nRooms are not available.\nPlease add the rooms first.";
+
+getch();    }
+
+else
+
+{   cout<<"Enter Room Number : ";
+
+cin>>rno;
+
+hm.checkOut(rno);   }
+
+break;
+
+case 6:
+
+hm.guestSummaryReport();      
+
+break;
+
+case 7:
+
+cout<<"\nTHANK YOU! FOR USING SOFTWARE";
+
+break;
+
+default:
+
+cout<<"\nPlease Enter correct option";
+
+break;
+
+}
+
+}while(opt!=7);
+
+ 
+
+getch();
+
+}
